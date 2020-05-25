31deeaa698b5dce5a1d39b2eb2c236c8a4143348
Trent Stanley <tstanley@lcs.on.ca>
2020-05-25 10:36:06 -0400

    Initial Commit

---
+++ Rubik's Timer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Rubik's Timer
+//
+//  Created by Stanley, Trent on 2020-05-25.
+//  Copyright © 2020 Stanley, Trent. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
