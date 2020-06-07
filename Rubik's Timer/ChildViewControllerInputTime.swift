db5e9c3fc419f1272e9418e53c9f442c37c2030e
Trent Stanley <tstanley@lcs.on.ca>
2020-06-07 13:50:30 -0400

    Created a parent view controller and a child view controller for the input time screen

---
+++ Rubik's Timer/ChildViewControllerInputTime.swift
@@ -0,0 +1,29 @@
+//
+//  ChildViewControllerInputTime.swift
+//  Rubik's Timer
+//
+//  Created by Stanley, Trent on 2020-06-07.
+//  Copyright © 2020 Stanley, Trent. All rights reserved.
+//
+
+import UIKit
+
+class ChildViewControllerInputTime: UIViewController {
+
+    // MARK: Properties
+    
+    @IBOutlet weak var timeSolved: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+    }
+    @IBAction func enterTime(_ sender: Any) {
+        
+    }
+    
+
+    
+    
+}
+
---
+++ Rubik's Timer/ParentViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ParentViewController.swift
+//  Rubik's Timer
+//
+//  Created by Stanley, Trent on 2020-06-07.
+//  Copyright © 2020 Stanley, Trent. All rights reserved.
+//
+
+import UIKit
+
+class ParentViewController: UIViewController {
+
+    // MARK: Properties
+    
+    
+
+    
+    
+}
