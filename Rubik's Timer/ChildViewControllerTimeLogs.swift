1f2d02d1177f02d26673905ba885bf0326d2bfb0
Trent Stanley <tstanley@lcs.on.ca>
2020-06-07 14:26:21 -0400

    Created a child view controller for the time logs

---
+++ Rubik's Timer/ChildViewControllerTimeLogs.swift
@@ -0,0 +1,13 @@
+//
+//  ChildViewControllerTimeLogs.swift
+//  Rubik's Timer
+//
+//  Created by Stanley, Trent on 2020-06-07.
+//  Copyright © 2020 Stanley, Trent. All rights reserved.
+//
+
+import UIKit
+
+class ChildViewControllerTimeLogs: UIViewController {
+
+    }
